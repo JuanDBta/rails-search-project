f1ea4f31eee74cf8fbfc7798c65676d49f8848f9
JuanDBta <simplebetsas@gmail.com>
2024-01-06 00:42:36 -0500

    Modified test to new models

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,21 +1,24 @@
 # En spec/models/search_spec.rb
 RSpec.describe Search, type: :model do
   it { is_expected.to validate_presence_of(:query) }
-  it { is_expected.to validate_presence_of(:ip_address) }
+  it { is_expected.to belong_to(:user) }
 
   it 'requires at least two spaces and three words in the query' do
-    should = Search.new(query: 'Valid search with three words', ip_address: '127.0.0.1')
+    user = User.create(ip_address: '127.0.0.1')
+    should = Search.new(query: 'Valid search with three words', user: user)
     expect(should).to be_valid
   end
 
   it 'rejects searches with less than two spaces in the query' do
-    should_not = Search.new(query: 'Invalid search', ip_address: '127.0.0.1')
+    user = User.create(ip_address: '127.0.0.1')
+    should_not = Search.new(query: 'Invalid search', user: user)
     expect(should_not).not_to be_valid
     expect(should_not.errors[:query]).to include('Please make a valid search')
   end
 
   it 'validates that the length of :query is at least 7' do
-    should_not = Search.new(query: 'Invalid', ip_address: '127.0.0.1')
+    user = User.create(ip_address: '127.0.0.1')
+    should_not = Search.new(query: 'Invalid', user: user)
     expect(should_not).not_to be_valid
     expect(should_not.errors[:query]).to include('Please make a valid search')
   end
--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Searches', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/searches/index'
+RSpec.describe "Searches", type: :request do
+  include FactoryBot::Syntax::Methods
+  let(:user) { create(:user) }
+
+  describe "GET /users/:user_id/searches" do
+    it "returns http success" do
+      get "/users/#{user.id}/searches"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/searches/create'
+  describe "POST /users/:user_id/searches" do
+    it "returns http success" do
+      post "/users/#{user.id}/searches", params: { search: { query: "a new example", user_id: user.id } }
       expect(response).to have_http_status(:success)
     end
   end
