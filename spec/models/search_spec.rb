5b0bf2e1b8057094ddfb049796fc5cc2c634889c
JuanDBta <simplebetsas@gmail.com>
2024-01-06 00:43:19 -0500

    Fixed offenses

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe Search, type: :model do
 
   it 'requires at least two spaces and three words in the query' do
     user = User.create(ip_address: '127.0.0.1')
-    should = Search.new(query: 'Valid search with three words', user: user)
+    should = Search.new(query: 'Valid search with three words', user:)
     expect(should).to be_valid
   end
 
   it 'rejects searches with less than two spaces in the query' do
     user = User.create(ip_address: '127.0.0.1')
-    should_not = Search.new(query: 'Invalid search', user: user)
+    should_not = Search.new(query: 'Invalid search', user:)
     expect(should_not).not_to be_valid
     expect(should_not.errors[:query]).to include('Please make a valid search')
   end
 
   it 'validates that the length of :query is at least 7' do
     user = User.create(ip_address: '127.0.0.1')
-    should_not = Search.new(query: 'Invalid', user: user)
+    should_not = Search.new(query: 'Invalid', user:)
     expect(should_not).not_to be_valid
     expect(should_not.errors[:query]).to include('Please make a valid search')
   end
--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Searches", type: :request do
+RSpec.describe 'Searches', type: :request do
   include FactoryBot::Syntax::Methods
   let(:user) { create(:user) }
 
-  describe "GET /users/:user_id/searches" do
-    it "returns http success" do
+  describe 'GET /users/:user_id/searches' do
+    it 'returns http success' do
       get "/users/#{user.id}/searches"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "POST /users/:user_id/searches" do
-    it "returns http success" do
-      post "/users/#{user.id}/searches", params: { search: { query: "a new example", user_id: user.id } }
+  describe 'POST /users/:user_id/searches' do
+    it 'returns http success' do
+      post "/users/#{user.id}/searches", params: { search: { query: 'a new example', user_id: user.id } }
       expect(response).to have_http_status(:success)
     end
   end
