5472a60b60bb416e8d6af435acee1a0310b2a9d4
JuanDBta <simplebetsas@gmail.com>
2024-01-05 21:55:07 -0500

    Created routes and Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  def create
+    @user = User.new(ip_address: request.remote_ip)
+
+    if @user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { error: "Unable to create user", details: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
