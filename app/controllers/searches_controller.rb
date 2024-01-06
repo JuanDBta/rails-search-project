6d4714cd22ccb1c99acf60c38670a16b37396f4c
JuanDBta <simplebetsas@gmail.com>
2024-01-06 13:27:40 -0500

    Adapted searches controller to input

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,13 +8,20 @@ class SearchesController < ApplicationController
   end
 
   def create
+    @user = User.find_or_create_by(ip_address: request.remote_ip)
     @search = Search.new(search_params)
 
-    if search_complete?(@search.query) && @search.save
+    if @search.save
       render json: @search, status: :created
     else
       render json: { error: 'Invalid search' }, status: :unprocessable_entity
     end
+  end
+
+  def show_user_searches
+    @user = User.find(params[:user_id])
+    @searches = @user.searches
+    render json: @searches, status: :ok
   end
 
   private
@@ -26,9 +33,5 @@ class SearchesController < ApplicationController
 
   def search_params
     params.require(:search).permit(:query).merge(user_id: @user.id)
-  end
-
-  def search_complete?(query)
-    query.length > 7
   end
 end
