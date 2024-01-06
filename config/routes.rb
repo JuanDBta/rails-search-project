7aef5cec10ee95f1051dd51cc8c0e452ab19e9da
JuanDBta <simplebetsas@gmail.com>
2024-01-06 13:58:09 -0500

    Added routes to handle post with ip_address as user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :index, :show] do
     resources :searches, only: [:create, :index]
   end
+  resources :searches, only: [:create, :index], on: :collection
 end
