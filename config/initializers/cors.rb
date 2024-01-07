f4d89da17013fd5b2695c32b018af068c01339d4
JuanDBta <simplebetsas@gmail.com>
2024-01-07 15:30:41 -0500

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://juandbta.github.io'
+      origins '*'
   
       resource "*",
         headers: :any,
