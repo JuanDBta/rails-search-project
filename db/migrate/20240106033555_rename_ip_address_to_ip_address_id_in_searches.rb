a2f768027c03e5565b908c2486e3799230653398
JuanDBta <simplebetsas@gmail.com>
2024-01-07 14:58:23 -0500

    Fixed migrations

--- db/migrate/20240106033555_rename_ip_address_to_ip_address_id_in_searches.rb
+++
@@ -1,5 +0,0 @@
-class RenameIpAddressToIpAddressIdInSearches < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :searches, :ip_address, :ip_address_id
-  end
-end
--- db/migrate/20240106034632_add_user_reference_to_searches.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReferenceToSearches < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :searches, :user, foreign_key: true
-  end
-end
