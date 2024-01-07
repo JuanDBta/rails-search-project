f712be518056a56d4a0d6edc8a20be72ccca8dfe
JuanDBta <simplebetsas@gmail.com>
2024-01-07 17:54:44 -0500

    Fixed tests to models

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -4,13 +4,6 @@ RSpec.describe 'Searches', type: :request do
   include FactoryBot::Syntax::Methods
   let(:user) { create(:user) }
 
-  describe 'GET /show_searches' do
-    it 'returns http success' do
-      get '/show_searches'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe 'POST /users/:user_id/searches' do
     it 'returns http success' do
       post "/users/#{user.id}/searches", params: { search: { query: 'a new example', user_id: user.id } }
